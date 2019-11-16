3fb0a3a6d53463f637e4c1f0e875acbf61458af7
dan <dan@pop-os.localdomain>
2019-11-16 22:58:27 +0700

    box

--- ScanText.py
+++ ScanText.py
@@ -1,4 +1,17 @@
+import pytesseract
 def GetText(listResult,listBigBox,img):
     result = []
+    if len(listBigBox)==0:
+        result.append(pytesseract.image_to_string(img,lang='vie'))
+        return result
+    bigBoxTemp = []
+    listYCoord = []
+    listYCoord.append(0)
+    (height,_) = img.shape[:2]
+    for (_,y,_,h) in listBigBox:
+        bigBoxTemp.append((y,y+h))
+        listYCoord.append(y)
+        listYCoord.append(y+h)
+    listYCoord.append(height)
     
     return result
